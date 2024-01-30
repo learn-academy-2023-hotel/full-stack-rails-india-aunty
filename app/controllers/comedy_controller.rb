159dd1c4d4776e95bd1fabd8d7f5d1ca8ab23f36
SunkissedQueen <baxtercm@outlook.com>
2024-01-29 21:27:34 -0800

    modified README to track changes for new/create restful routes

--- app/controllers/comedy_controller.rb
+++ app/controllers/comedy_controller.rb
@@ -9,4 +9,20 @@ class ComedyController < ApplicationController
     @one_joke = Comedy.find(params[:id])
   end
 
+  # new instance
+  def new
+    @new_joke = Comedy.new
+  end
+
+  def create
+    @new_joke = Comedy.create(joke_params)
+    if @new_joke.valid?
+      redirect_to comedies_path
+    end
+  end
+
+  private
+  def joke_params
+    params.require(:comedy).permit(:joke, :punch_line)
+  end
 end
